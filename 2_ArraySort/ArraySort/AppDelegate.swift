271ec355e3111fe4b952cba5872f4495289c1fc6
Sean Robinson <Sean.Robinson@LexAble.com>
2015-03-29 22:29:24 +0100

    Inital commit for ch2

---
+++ 2_ArraySort/ArraySort/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ArraySort
+//
+//  Created by Sean Robinson on 29/03/2015.
+//  Copyright (c) 2015 SRobinson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ 2_ArraySort/ArraySort/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ArraySort
+//
+//  Created by Sean Robinson on 29/03/2015.
+//  Copyright (c) 2015 SRobinson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
