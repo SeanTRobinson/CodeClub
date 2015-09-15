d63233b33beb06f29eb86ee49d580644978d45b5
Sean Robinson <Sean.Robinson@LexAble.com>
2015-09-15 21:48:53 +0100

    Added pseudo for trainer

---
+++ 4_RiseOfTheBots/app/src/main/java/com/seanrobinson/authorbot/LexCreator.java
@@ -0,0 +1,22 @@
+package com.seanrobinson.authorbot;
+
+public class LexCreator {
+
+    public LexCreator() {
+
+    }
+
+    public void createLexicon(String inputPath, String outputPath) {
+        /*
+        open file
+        add each word in file to list
+        convert list to set
+        for each word in set: (1)
+          for each word in list: (2)
+            if 2 == 1 and 2[-1] is valid word:
+              add 2[+1] to new list
+            1.followers = new list
+        write json to file
+         */
+    }
+}
